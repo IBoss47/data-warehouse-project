0c703c99fd412600c0bb4e2ffe81665548c5ee75
Thanin Tangkoblap <work.thanin@gmail.com>
2026-09-24 12:56:05 +0700

    [main] ddl: create ddl scripts for table in crm and add data warehouse timestamp for each table, for incremental load

---
+++ scripts/bronze/erp/ddl_bronze__erp.sql
@@ -0,0 +1,38 @@
+if OBJECT_ID('bronze.erm_loc_a101', 'U') is null
+begin
+    create table bronze.erp_loc_a101(
+        cid NVARCHAR(50),
+        cntry NVARCHAR(50),
+
+        dwh_create_at datetime default GETDATE(),
+        dwh_update_at datetime default GETDATE()
+    );
+end;
+go
+
+if OBJECT_ID('bronze.erp_cust_az12', 'U') is null
+begin
+    CREATE TABLE bronze.erp_cust_az12 (
+        cid    NVARCHAR(50),
+        bdate  DATE,
+        gen    NVARCHAR(50),
+
+        dwh_create_at datetime default GETDATE(),
+        dwh_update_at datetime default GETDATE()
+    );
+end;
+go
+
+if OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') is null
+begin
+    CREATE TABLE bronze.erp_px_cat_g1v2 (
+        id           NVARCHAR(50),
+        cat          NVARCHAR(50),
+        subcat       NVARCHAR(50),
+        maintenance  NVARCHAR(50),
+
+        dwh_create_at datetime default GETDATE(),
+        dwh_update_at datetime default GETDATE()
+    );
+end;
+go
